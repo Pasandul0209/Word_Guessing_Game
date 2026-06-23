9bd22d0d81f7591378cab347f6591f69eb9637bc
Pasandul Kasthuri <nethmika86@gmail.com>
2026-06-23 09:41:02 +0530

    Fix typo in loading status message

--- app/src/main/java/com/kasthuri/word_guessing_game/MainActivity.java
+++ app/src/main/java/com/kasthuri/word_guessing_game/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void fetchNewWord() {
         resetGameState();
-        tvStatus.setText("Loading a ne word...");
+        tvStatus.setText("Loading a new word...");
 
         String url = "https://random-word-api.herokuapp.com/word?length=" + getWordLength();
 
