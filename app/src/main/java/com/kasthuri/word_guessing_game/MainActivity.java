97425eb0536b4cf3fbf808c1c2d1c8ae70a82aa4
Pasandul Kasthuri <nethmika86@gmail.com>
2026-07-20 10:12:50 +0530

    Remove commented line in MainActivity

--- app/src/main/java/com/kasthuri/word_guessing_game/MainActivity.java
+++ app/src/main/java/com/kasthuri/word_guessing_game/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Running
+        
         SharedPreferences prefs = getSharedPreferences("WordGame", MODE_PRIVATE);
         playerName = prefs.getString("playerName", "Player");
 
